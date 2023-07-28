e25b162e8e0df6c167a1323f9b6fb1a7be50c496
Aman Prajapati <disketteit@gmail.com>
2023-07-28 06:05:13 -0700

    Added JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const AddQual = () => {
+    qualtag = document.getElementById("qc").innerHTML
+    document.getElementById("qc").innerHTML = qualtag + '<input type="text" placeholder="Qualification">'
+}
+
+const AddWexp = () => {
+    qualtag = document.getElementById("we").innerHTML
+    document.getElementById("we").innerHTML = qualtag + '<input type="text" placeholder="Qualification">'
+}
