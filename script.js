65019eab63755ce018c607032cc9afc5d13bff6d
Aman Prajapati <disketteit@gmail.com>
2023-07-28 06:49:22 -0700

    Started Resume Work

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const AddQual = () => {
-    qualtag = document.getElementById("qc").innerHTML
-    document.getElementById("qc").innerHTML = qualtag + '<input type="text" placeholder="Qualification">'
+    qualtag = document.getElementById("qc").innerHTML;
+    document.getElementById("qc").innerHTML = qualtag + '<input type="text" placeholder="Qualification">';
 }
 
 const AddWexp = () => {
-    qualtag = document.getElementById("we").innerHTML
-    document.getElementById("we").innerHTML = qualtag + '<input type="text" placeholder="Qualification">'
+    qualtag = document.getElementById("we").innerHTML;
+    document.getElementById("we").innerHTML = qualtag + '<input type="text" placeholder="Qualification">';
 }
+
+const generateres = () => {
+    
+} 
